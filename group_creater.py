5b353292c23ea5b04de56d500da09fdf6bc583b0
Griff Perry <griff.perry@adtran.com>
2023-02-15 22:53:37 -0600

    Improve efficiency, add co-leader

--- group_creater.py
+++ group_creater.py
@@ -36,9 +36,10 @@ class PlanningCenterBot():
     
     def group_init(self, group):
         self.create_group(group)
-        self.add_leader_to_group(group.get("leader"))
-        # self.add_co_leader_to_group(group.get("co-leader"))
-        # self.apply_group_settings(group)
+        self.add_leader_to_group(group)
+        if group.get("co-leader"):
+            self.add_member_to_group(group, leader=True)
+        # self.add_group_settings(group)
         self.return_out_to_main_groups_page()
 
     def go_to_main_groups_page(self):
@@ -53,22 +54,78 @@ class PlanningCenterBot():
         self.add_text_to_field(By.NAME, "group[name]", group.get("name"))
         self.click_button(By.XPATH, "/html/body/div[2]/div/div[3]/button/span")
 
-    def add_leader_to_group(self, member):
-        add_member_xpath = "/html/body/main/div/div/div[2]/div/div/div/div[3]/div[2]/div/div"
-        self.click_button(By.XPATH, add_member_xpath)
+    def add_leader_to_group(self, group):
+        add_first_member_xpath = "/html/body/main/div/div/div[2]/div/div/div/div[3]/div[2]/div/div"
+        self.click_button(By.XPATH, add_first_member_xpath)
+        self.search_for_member(group["leader"])
+        self.promote_member_to_leader(group, group["leader"])
+
+    def add_member_to_group(self, group, leader=False):
+        if leader:
+            # breakpoint()
+            self.click_button(By.XPATH, "//*[@id='group-member-finder']/div/div[3]/div[2]/div")
+            self.search_for_member(group["co-leader"])
+            self.promote_member_to_leader(group, group["co-leader"])
+
+    def search_for_member(self, member):
         self.add_text_to_field(By.ID, "person_search", member)
-        sleep(3) # Wait for search to complete
-        select_member_xpath = "/html/body/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/ul[1]/li/button"
-        self.click_button(By.XPATH, select_member_xpath)
-        self.click_button(By.XPATH, "/html/body/div[2]/div/div[3]/button[2]/span")
-        # Still have to make leader
+        only_result_xpath = "/html/body/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/ul[1]/li/button"
+        success = self.click_button(By.XPATH, only_result_xpath)
+        if not success:
+            # TODO: Use email to verify member when multiple members found.
+            print(f"More than one result when searching for {member}")
+            return
+        self.dont_notify_by_email()
+        self.click_button(By.XPATH, "/html/body/div[2]/div/div[3]/button[2]")
+
+    def promote_member_to_leader(self, group, member):
+        member_type = list(group.keys())[list(group.values()).index(member)]
+        if "leader" == member_type:
+            promote_xpath = "//*[@id='group-member-finder']/div/div[5]/div[2]/div/div[5]/div/div"
+        elif "co-leader" == member_type:
+            promote_xpath = "//*[@id='group-member-finder']/div/div[5]/div[2]/div[2]/div[5]/div/div"
+        else:
+            print(f"Can't find promote button for {member}")
+            return
+        self.click_button(By.XPATH, promote_xpath)
+        self.dont_notify_by_email()
+        self.click_button(By.XPATH, "/html/body/div[2]/div/div[3]/button[2]")
+
+    def dont_notify_by_email(self):
+        self.click_button(By.XPATH, "/html/body/div[2]/div/div[2]/form/div/label")
 
     def click_button(self, by_type, xpath):
-        self.driver.find_element(by_type, xpath).click()
-        sleep(1)
+        button = self.attempt_find_element(by_type, xpath)
+        if button:
+            button.click()
+            sleep(0.5)
+            return True
+        else:
+            print(f"Could not find button at {xpath}")
+            return False
 
-    def add_text_to_field(self, by_type, class_name, text):
-        self.driver.find_element(by_type, class_name).send_keys(text)
+    def add_text_to_field(self, by_type, xpath, text):
+        field = self.attempt_find_element(by_type, xpath)
+        if field:
+            field.send_keys(text)
+            return True
+        else:
+            print(f"Could not find field at {xpath}")
+            return False
+
+    def attempt_find_element(self, by_type, xpath, timeout=3):
+        element = None
+        sleep_time = 0.5
+        max_tries = timeout/sleep_time
+        tries = 0
+        while tries < max_tries:
+            try:
+                element = self.driver.find_element(by_type, xpath)
+                break
+            except Exception:
+                tries += 1
+                sleep(sleep_time)
+        return element
 
     def close_session(self):
         print("Closing session")
@@ -89,7 +146,7 @@ def main():
         1: {
             "name": "test bot 1",
             "leader": "Griff Perry",
-            "co-leader": None,
+            "co-leader": "Kaylee Perry",
             "type": "small groups",
             "schedule": "Thursday @ 11:30 AM Weekly",
             "description": "Test description",
