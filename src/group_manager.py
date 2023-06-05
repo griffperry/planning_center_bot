e55a4f5ad379cdb6daa0d77b52555656629e2847
Griff Perry <lgp@auburn.edu>
2023-06-05 15:29:13 -0500

    Fix vague tags names

--- src/group_manager.py
+++ src/group_manager.py
@@ -44,7 +44,7 @@ class GroupManager(StatusReport):
             success = self.add_group(group)
             if success:
                 for member in group["members"].values():
-                    self.add_member_to_group(member)
+                    self.add_member_to_group(group_name, member)
                 self.add_group_settings(group)
                 self.add_group_status(group_name, f"(User {self.id}) Group '{group_name}' created.")
             self.reports.append(self.create_report(group_name))
@@ -66,7 +66,7 @@ class GroupManager(StatusReport):
             return True
         return False
 
-    def add_member_to_group(self, member):
+    def add_member_to_group(self, group_name, member):
         member_name = member.get("name")
         member_status = member.get("status")
         member_email = member.get("email")
@@ -76,6 +76,8 @@ class GroupManager(StatusReport):
             person_obj = self.backend.people.query(qe)
             if len(person_obj) == 1:
                 self.current_group.add_member(person_id=person_obj[0].id, leader=promote_member)
+            else:
+                self.add_group_caveat(group_name, f"(User {self.id}) {member_name} not found when added to '{group_name}'.")
 
     def add_group_settings(self, group):
         with self.current_group.no_refresh():
@@ -132,7 +134,14 @@ class GroupManager(StatusReport):
         if tags:
             tag_list = self.gen_simple_tag_list(tags)
             for tag in tag_list:
-                self.current_group.add_tag(tag)
+                try:
+                    self.current_group.add_tag(tag)
+                except:
+                    tag_data = self.backend.groups.tags.query(tag)
+                    for tag_element in tag_data:
+                        if tag_element.attributes.name == tag:
+                            self.current_group.add_tag(tag_element)
+                            break
 
     def gen_simple_tag_list(self, tags):
         tag_list = []
