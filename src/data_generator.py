0ab4077d1da7db1a4c6ba6f76f211326f25a2050
griffperry <40665653+griffperry@users.noreply.github.com>
2024-01-23 19:50:02 -0600

    Update data gen to handle variations of spreadsheet (#29)
    * begin testing of data gen
    
    * finish tests for schedule and addresses
    
    * add pytest to requirements
    
    * finish tests for gen functions, play code golf
    
    ---------
    
    Co-authored-by: Griff Perry <griff.perry@adtran.com>
    Co-authored-by: Griff Perry <lgp@auburn.edu>

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,28 +1,29 @@
 import pandas as pd
+import re
 from datetime import datetime
 from enum import Enum
 
 
 class Fields(Enum):
 
-    LEADER_FIRST_NAMES = "Unnamed: 1"
-    LEADER_LAST_NAMES = "Unnamed: 2"
-    LEADER_EMAILS = "Unnamed: 3"
-    LEADER_ADDRESSES = "Unnamed: 7"
-    GROUP_CAMPUSES = "Unnamed: 8"
-    CO_LEADER_NAMES = "Unnamed: 11"
-    CO_LEADER_EMAILS = "Unnamed: 12"
-    GROUP_NAMES = "Unnamed: 15"
-    GROUP_DESCRIPTIONS = "Unnamed: 16"
-    GROUP_AGE_RANGES = "Unnamed: 17"
-    GROUP_GENDERS = "Unnamed: 18"
-    GROUP_MEET_TYPES = "Unnamed: 19"
-    GROUP_OUT_HOME_ADDRESSES = "Unnamed: 20"
-    GROUP_OCCURRENCES = "Unnamed: 21"
-    GROUP_DAYS = "Unnamed: 22"
-    GROUP_TIMES = "Unnamed: 23"
-    GROUP_TYPES = "Unnamed: 24"
-    GROUP_CHILDCARE = "Unnamed: 25"
+    LEADER_FIRST_NAMES = "First Name"
+    LEADER_LAST_NAMES = "Last Name"
+    LEADER_EMAILS = "Email"
+    LEADER_ADDRESSES = "Address"
+    GROUP_CAMPUSES = "What Daystar Campus do you attend?"
+    CO_LEADER_NAMES = "Co-Leader Names"
+    CO_LEADER_EMAILS = "Co-Leader Emails"
+    GROUP_NAMES = "Group Name"
+    GROUP_DESCRIPTIONS = "Group Description"
+    GROUP_AGE_RANGES = "Age Range?"
+    GROUP_GENDERS = "Group Members to be:"
+    GROUP_MEET_TYPES = "How will your Small Group meet?"
+    GROUP_OUT_HOME_ADDRESSES = "Where will your Group meet? (If address other than your home, just type in address)"
+    GROUP_OCCURRENCES = "How often will your Group meet? "
+    GROUP_DAYS = "What day will your Group meet?"
+    GROUP_TIMES = "What time will your Group meet? (Specify am or pm)"
+    GROUP_TYPES = "Type of Small Group (check all that apply)"
+    GROUP_CHILDCARE = "Will Childcare be provided?"
 
 
 class DataGenerator():
@@ -32,12 +33,22 @@ class DataGenerator():
         self.num_groups = 0
         self.app_run = app_run
         self.num_test_groups = 6
+        self.campus = "Madison"
 
     def verify_data(self, data_file):
+        data_object = self.get_excel_sheet_object(data_file)
+        self.translate_data(data_object)
+        return True
+
+    def get_excel_sheet_object(self, data_file):
         file_object = pd.read_excel(data_file)
         file_object = file_object.fillna('')
-        self.translate_data(file_object)
-        return True
+        campus_data = self.filter_on_campus(file_object)
+        return campus_data
+
+    def filter_on_campus(self, file_object):
+        file_object = file_object.loc[file_object[Fields.GROUP_CAMPUSES.value] == self.campus]
+        return file_object
 
     def translate_data(self, data_object):
         groups = {}
@@ -68,49 +79,117 @@ class DataGenerator():
         for i in range(self.num_groups):
             self.data[i] = groups[i]
 
-    def _gen_data_list(self, data_object, column):
-        return data_object[column].to_list()[1:]
+    def _gen_data_list(self, data_object, column_name):
+        return data_object[column_name].to_list()
 
     def _gen_members_data(self, index, groups, data_object):
         leader_first_names = self._gen_data_list(data_object, Fields.LEADER_FIRST_NAMES.value)
         leader_last_names = self._gen_data_list(data_object, Fields.LEADER_LAST_NAMES.value)
         leader_emails = self._gen_data_list(data_object, Fields.LEADER_EMAILS.value)
-        co_leader_names = self._gen_data_list(data_object, Fields.CO_LEADER_NAMES.value)
-        co_leader_emails = self._gen_data_list(data_object, Fields.CO_LEADER_EMAILS.value)
+        co_leader_names = []
+        co_leader_emails = []
         groups[index]["members"] = {
             0: {
                 "name": f"{leader_first_names[index].strip()} {leader_last_names[index].strip()}",
                 "status": "leader",
-                "email": f"{leader_emails[index].strip()}" if leader_emails[index] else None,
+                "email": leader_emails[index].strip() if leader_emails[index] else None,
             },
-            1: {
-                "name": f"{co_leader_names[index].strip()}",
-                "status": "co-leader",
-                "email": f"{co_leader_emails[index].strip()}" if co_leader_emails[index] else None,
-            }
         }
 
+        def format_co_leader_info(info):
+            if "and" in info:
+                return info.split("and")
+            if "//" in info:
+                return info.split("//")
+            return info
+
+        for co_leader_name in self._gen_data_list(data_object, Fields.CO_LEADER_NAMES.value):
+            co_leader_names.append(format_co_leader_info(co_leader_name))
+        for co_leader_email in self._gen_data_list(data_object, Fields.CO_LEADER_EMAILS.value):
+            co_leader_emails.append(format_co_leader_info(co_leader_email))
+
+        co_leader_name = co_leader_names[index]
+        co_leader_email = co_leader_emails[index]
+        if isinstance(co_leader_name, list) and isinstance(co_leader_email, list):
+            for co_index, co_leader_name in enumerate(co_leader_name, 1):
+                groups[index]["members"][co_index] = {}
+                groups[index]["members"][co_index]["name"] = co_leader_name.strip()
+                groups[index]["members"][co_index]["status"] = "co-leader"
+            for co_index, co_leader_email in enumerate(co_leader_email, 1):
+                groups[index]["members"][co_index]["email"] = co_leader_email.strip()
+        else:
+            groups[index]["members"][1] = {
+                    "name": co_leader_name.strip(),
+                    "status": "co-leader",
+                    "email": co_leader_email.strip() if co_leader_email else None,
+                }
+
     def _gen_schedule_data(self, idx, groups, data_object):
-        group_occurences = self._gen_data_list(data_object, Fields.GROUP_OCCURRENCES.value)
+        group_occurrences = self._gen_data_list(data_object, Fields.GROUP_OCCURRENCES.value)
         group_days = self._gen_data_list(data_object, Fields.GROUP_DAYS.value)
         group_times = self._gen_data_list(data_object, Fields.GROUP_TIMES.value)
-        groups[idx]["schedule"] = f"{group_days[idx]} @ {group_times[idx]} {group_occurences[idx]}"
+        self._format_group_days(group_days)
+        self._format_group_times(group_times)
+        groups[idx]["schedule"] = f"{group_days[idx]} @ {group_times[idx]} {group_occurrences[idx]}"
+
+    def _format_group_days(self, group_days):
+        for idx, set_days in enumerate(group_days):
+            day_string = ""
+            days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday",
+                                "Friday", "Saturday", "Sunday"]
+            for day in days_of_the_week:
+                if day in set_days:
+                    day_string += f"{day}, "
+            group_days[idx] = day_string.rstrip(", ")
+
+    def _format_group_times(self, group_times):
+        for idx, time in enumerate(group_times):
+            group_times[idx] = "TBD"
+            time_period = "AM" if "a" in time.lower() else "PM"
+            result = re.search(r'\d{2}', time)
+            all_nums = re.findall(r'\d', time)
+            hour = all_nums[0] if all_nums else None
+            if result and result.group(0) == f"{all_nums[0]}{all_nums[1]}":
+                if all_nums[0] == "1" and all_nums[1] in ["0", "1", "2"]:
+                    hour = result.group(0)
+                elif all_nums[0] == "0":
+                    hour = all_nums[1]
+            minute = "00"
+            if all_nums:
+                if len(all_nums) > 1:
+                    hour_past = False
+                    for val in all_nums:
+                        if val == "3":
+                            if hour == "3" and not hour_past:
+                                hour_past = True
+                            else:
+                                minute = "30"
+                                break
+                group_times[idx] = f"{hour}:{minute} {time_period}"
 
     def _gen_address_data(self, index, groups, data_object):
         groups_meet_type = self._gen_data_list(data_object, Fields.GROUP_MEET_TYPES.value)
         groups_out_home_address = self._gen_data_list(data_object, Fields.GROUP_OUT_HOME_ADDRESSES.value)
         leader_addresses = self._gen_data_list(data_object, Fields.LEADER_ADDRESSES.value)
-        if groups_meet_type[index] == "In Home":
+        groups[index]["address"] = groups_out_home_address[index]
+        if "In Home" in groups_meet_type[index] and groups_out_home_address[index] == '':
             groups[index]["address"] = leader_addresses[index].strip("Home:/\n")
-        elif groups_meet_type[index] != "Online":
-            groups[index]["address"] = groups_out_home_address[index]
-        else:
-            groups[index]["address"] = None
+        self._format_group_address(index, groups)
+
+    def _format_group_address(self, index, groups):
+        if groups[index]["address"]:
+            address = groups[index]["address"]
+            first_address_value = address.split(" ")[0]
+            address_number = re.search("\d+", first_address_value)
+            groups[index]["address"] = ''
+            if address_number is not None:
+                groups[index]["address"] = address.replace("\n", " ")
 
     def _gen_tags_data(self, index, groups, data_object):
         group_campuses = self._gen_data_list(data_object, Fields.GROUP_CAMPUSES.value)
         group_occurences = self._gen_data_list(data_object, Fields.GROUP_OCCURRENCES.value)
         group_days = self._gen_data_list(data_object, Fields.GROUP_DAYS.value)
+        self._format_group_days(group_days)
         group_genders = self._gen_data_list(data_object, Fields.GROUP_GENDERS.value)
         groups[index]["tags"] = {
             "campus": group_campuses[index],
@@ -121,7 +200,7 @@ class DataGenerator():
             "group type": self._get_types(index, data_object),
             "group age": self._get_age_range(index, data_object),
             "group members": group_genders[index],
-            "day of week": group_days[index],
+            "day of week": [group_days[index]],
         }
 
     def _get_year(self):
@@ -139,30 +218,33 @@ class DataGenerator():
         attributes = []
         groups_meet_type = self._gen_data_list(data_object, Fields.GROUP_MEET_TYPES.value)
         group_childcare = self._gen_data_list(data_object, Fields.GROUP_CHILDCARE.value)
-        if groups_meet_type[index] == "Online":
+        if "Online" in groups_meet_type[index]:
             attributes.append("Online group")
         if group_childcare[index].lower() == "yes":
             attributes.append("Childcare Available")
         return attributes
 
     def _get_types(self, index, data_object):
+        def remove_whitespace(val):
+            return val.replace(" ", "") if "/" in val else val.strip()
+
         all_group_types = self._gen_data_list(data_object, Fields.GROUP_TYPES.value)
         group_types = all_group_types[index]
+        formatted_group_types = []
         if "," in group_types:
-            group_types_list = group_types.split(",")
-            group_types = []
-            for group_type in group_types_list:
-                group_types.append(group_type.strip())
-            return group_types
-        return [group_types]
+            for group_type in group_types.split(","):
+                formatted_group_types.append(remove_whitespace(group_type))
+        else:
+            formatted_group_types.append(remove_whitespace(group_types))
+        return formatted_group_types
 
     def _get_age_range(self, index, data_object):
         all_age_ranges = self._gen_data_list(data_object, Fields.GROUP_AGE_RANGES.value)
         age_ranges = all_age_ranges[index]
+        formatted_age_ranges = []
         if "," in age_ranges:
-            age_range_list = age_ranges.split(",")
-            age_ranges = []
-            for age_range in age_range_list:
-                age_ranges.append(age_range.strip())
-            return age_ranges
-        return [age_ranges]
+            for age_range in age_ranges.split(","):
+                formatted_age_ranges.append(age_range.strip())
+        else:
+            formatted_age_ranges.append(age_ranges.strip())
+        return formatted_age_ranges
---
+++ test/test_data_generator.py
@@ -0,0 +1,273 @@
+import pytest
+import src.data_generator as dg
+
+
+@pytest.fixture(scope="function")
+def generator():
+    return dg.DataGenerator()
+
+
+@pytest.mark.parametrize(
+    "index,expected",
+    [
+        (0, {
+            0: {
+                'members': {
+                    0: {
+                        'name': 'Gracie Manley',
+                        'status': 'leader',
+                        'email': 'gracieminton12@gmail.com'
+                    },
+                    1: {
+                        'name': 'Hannah Manley',
+                        'status': 'co-leader',
+                        'email': 'hboaz03@gmail.com'
+                    }
+                }
+            }
+        }),
+        (1, {
+            1: {
+                'members': {
+                    0: {
+                        'name': 'Michelle Thao',
+                        'status': 'leader',
+                        'email': 'itsmischh@gmail.com'
+                    },
+                    1: {
+                        'name': '',
+                        'status': 'co-leader',
+                        'email': None
+                    },
+                }
+            }
+        }),
+        (2, {
+            2: {
+                'members': {
+                    0: {
+                        'name': 'David Groseclose',
+                        'status': 'leader',
+                        'email': 'dbgrosec@gmail.com'
+                    },
+                    1: {
+                        'name': 'Griff Perry',
+                        'status': 'co-leader',
+                        'email': None
+                    }
+                }
+            }
+        }),
+        (8, {
+            8: {
+                'members': {
+                    0: {
+                        'name': 'Alyssa Detwiler',
+                        'status': 'leader',
+                        'email': 'alyssadetwiler@daystarchurch.tv'
+                    },
+                    1: {
+                        'name': 'Kūpono Detwiler',
+                        'status': 'co-leader',
+                        'email': 'kupono09@gmail.com'
+                    },
+                    2: {
+                        'name': 'Gracie Manley',
+                        'status': 'co-leader',
+                        'email': 'gracieminton12@gmail.com'
+                    }
+                }
+            }
+        }),
+        (13, {
+            13: {
+                'members': {
+                    0: {
+                        'name': 'Chris Blue',
+                        'status': 'leader',
+                        'email': 'cblue0241@gmail.com'
+                    },
+                    1: {
+                        'name': 'Garrett Manley',
+                        'status': 'co-leader',
+                        'email': 'garrettmanley13@yahoo.com'
+                    },
+                    2: {
+                        'name': 'Zane Manley',
+                        'status': 'co-leader',
+                        'email': 'manleyman456@gmail.com'
+                    }
+                }
+            }
+        }),
+    ],
+)
+def test_gen_members_data(index, expected, generator: dg.DataGenerator):
+    data_object = generator.get_excel_sheet_object("test/groups2.xlsx")
+    groups = {}
+    groups[index] = {}
+
+    generator._gen_members_data(index, groups, data_object)
+    print(groups)
+    assert groups == expected
+
+
+@pytest.mark.parametrize(
+    "index,expected",
+    [
+        (0, {0: {'schedule': 'Thursday @ 6:00 PM Weekly'}}),
+        (1, {1: {'schedule': 'Friday @ 5:30 PM Weekly'}}),
+        (2, {2: {'schedule': 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday @ 6:30 AM Weekly'}}),
+        (3, {3: {'schedule': 'Monday @ 6:00 PM Weekly'}}),
+        (4, {4: {'schedule': 'Monday, Friday, Saturday @ TBD Weekly'}}),
+        (5, {5: {'schedule': 'Sunday @ 3:30 PM Weekly'}}),
+        (6, {6: {'schedule': 'Wednesday @ 6:30 PM Weekly'}}),
+        (7, {7: {'schedule': 'Friday @ 6:00 PM Weekly'}}),
+        (8, {8: {'schedule': 'Friday @ 6:00 PM Bi-weekly'}}),
+        (9, {9: {'schedule': 'Thursday @ 7:00 PM Monthly'}}),
+        (10, {10: {'schedule': 'Tuesday @ 6:00 PM Weekly'}}),
+        (11, {11: {'schedule': 'Saturday @ 1:00 PM Bi-weekly'}}),
+        (14, {14: {'schedule': 'Friday @ 7:00 AM Weekly'}}),
+    ],
+)
+def test_gen_schedule_data(index, expected, generator: dg.DataGenerator):
+    data_object = generator.get_excel_sheet_object("test/groups2.xlsx")
+    groups = {}
+    groups[index] = {}
+
+    generator._gen_schedule_data(index, groups, data_object)
+    print(groups)
+    assert groups == expected
+
+
+@pytest.mark.parametrize(
+    "index,expected",
+    [
+        (0, {0: {'address': '28541 State Highway 251 Ardmore, AL 35739'}}),
+        (1, {1: {'address': ''}}),
+        (2, {2: {'address': ''}}),
+        (3, {3: {'address': '712 Ridgegate Place 712 Huntsville, AL 35801'}}),
+        (4, {4: {'address': ''}}),
+        (5, {5: {'address': ''}}),
+        (6, {6: {'address': '12364 Burgreen Rd. Madison, AL.'}}),
+        (7, {7: {'address': '113 WELLINGTON DR, MADISON'}}),
+        (8, {8: {'address': '137 Greenwood Circle Harvest, AL 35749'}}),
+        (9, {9: {'address': ''}}),
+        (10, {10: {'address': '426 Ripple Lake Dr Sw Huntsville, AL 35824'}}),
+        (11, {11: {'address': ''}}),
+        (13, {13: {'address': '2609 LegacyPreserve, Brownboro, AL 35741'}}),
+    ],
+)
+def test_gen_address_data(index, expected, generator: dg.DataGenerator):
+    data_object = generator.get_excel_sheet_object("test/groups2.xlsx")
+    groups = {}
+    groups[index] = {}
+
+    generator._gen_address_data(index, groups, data_object)
+    print(groups)
+    assert groups == expected
+
+
+@pytest.mark.parametrize(
+    "index,expected",
+    [
+        (0, {
+            0: {
+                'tags': {
+                    'campus': 'Madison',
+                    'year': '2023',
+                    'season': 'Fall',
+                    'regularity': 'Weekly',
+                    'group attributes': [],
+                    'group type': ['Freedom'],
+                    'group age': ['18-30'],
+                    'group members': 'Women',
+                    'day of week': ['Thursday'],
+                }
+            }
+        }),
+        (1, {
+            1: {
+                'tags': {
+                    'campus': 'Madison',
+                    'year': '2023',
+                    'season': 'Fall',
+                    'regularity': 'Weekly',
+                    'group attributes': [],
+                    'group type': ['Fitness/Health'],
+                    'group age': ['18-30', '30-55', '55+'],
+                    'group members': 'Women',
+                    'day of week': ['Friday'],
+                }
+            }
+        }),
+        (2, {
+            2: {
+                'tags': {
+                    'campus': 'Madison',
+                    'year': '2023',
+                    'season': 'Fall',
+                    'regularity': 'Weekly',
+                    'group attributes': [],
+                    'group type': ['Prayer', 'Fitness/Health', 'Fun/Hangout'],
+                    'group age': ['All ages welcome'],
+                    'group members': 'Men',
+                    'day of week': ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday'],
+                }
+            }
+        }),
+        (5, {
+            5: {
+                'tags': {
+                    'campus': 'Madison',
+                    'year': '2023',
+                    'season': 'Fall',
+                    'regularity': 'Weekly',
+                    'group attributes': [],
+                    'group type': ['Book Study', 'Bible Study'],
+                    'group age': ['All ages welcome'],
+                    'group members': 'Co-ed (Both Men and Women welcome)',
+                    'day of week': ['Sunday'],
+                }
+            }
+        }),
+        (8, {
+            8: {
+                'tags': {
+                    'campus': 'Madison',
+                    'year': '2023',
+                    'season': 'Fall',
+                    'regularity': 'Bi-weekly',
+                    'group attributes': ['Childcare Available'],
+                    'group type': ['Fun/Hangout'],
+                    'group age': ['18-30'],
+                    'group members': 'Co-ed (Both Men and Women welcome)',
+                    'day of week': ['Friday'],
+                }
+            }
+        }),
+        (9, {
+            9: {
+                'tags': {
+                    'campus': 'Madison',
+                    'year': '2023',
+                    'season': 'Fall',
+                    'regularity': 'Monthly',
+                    'group attributes': ["Online group"],
+                    'group type': ['Fun/Hangout'],
+                    'group age': ['All ages welcome'],
+                    'group members': 'Co-ed (Both Men and Women welcome)',
+                    'day of week': ['Thursday'],
+                }
+            }
+        }),
+    ],
+)
+def test_gen_tags_data(index, expected, generator: dg.DataGenerator):
+    data_object = generator.get_excel_sheet_object("test/groups2.xlsx")
+    groups = {}
+    groups[index] = {}
+
+    generator._gen_tags_data(index, groups, data_object)
+    print(groups)
+    assert groups == expected
